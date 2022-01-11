c092bfc4a18e4dcb634a4dfe209d9a485ba2d028
Alexander <alexandr.bechko@gmail.com>
2022-01-11 04:18:36 +0300

    added reading text from a file

--- src/com/company/collections/task/optional/task4/Main.java
+++ src/com/company/collections/task/optional/task4/Main.java
@@ -1,26 +1,42 @@
 package com.company.collections.task.optional.task4;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    private final static String POEM =
-            "Я помню чудное мгновенье:\n" +
-            "Передо мной явилась ты,\n" +
-            "Как мимолетное виденье,\n" +
-            "Как гений чистой красоты.\n" +
-            "В томленьях грусти безнадежной,\n" +
-            "В тревогах шумной суеты,\n" +
-            "Звучал мне долго голос нежный\n" +
-            "И снились милые черты.";
-
     public static void main(String[] args) {
-        List<String> list = Arrays.stream(POEM.split("\n")).toList();
-        System.out.println(list);
+        String poem = readFromFile("src\\resources\\poem.txt");
 
+        System.out.println("----Before----");
+        List<String> list = Arrays.stream(poem.split("\n")).toList();
+        outputList(list);
+
+        System.out.println("----After----");
         list = list.stream().sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
-        System.out.println(list);
+        outputList(list);
+    }
+
+    private static String readFromFile(String path) {
+        StringBuilder readString = new StringBuilder();
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            while (line != null) {
+                readString.append(line).append("\n");
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return readString.toString();
+    }
+
+    private static <T> void outputList(List<T> list) {
+        for (T element : list)
+            System.out.println(element.toString());
     }
 }
